2c6c13eb6a7ff501f0782c074791696bb1fe342c
Emmanuel Eugene Ofori <eofori300@yahoo.com>
2025-01-29 16:02:14 +0000

    Adds initial project setup files

---
+++ fantasy-league/src/main/java/com/codemora/fantasy_league/FantasyLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.codemora.fantasy_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyLeagueApplication.class, args);
+	}
+
+}
---
+++ fantasy-league/src/test/java/com/codemora/fantasy_league/FantasyLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemora.fantasy_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
